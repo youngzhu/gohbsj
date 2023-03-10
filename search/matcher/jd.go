50c2d342398797294fb7a8e3e505a17e0e31147b
young-win <young.zhu.2012@gmail.com>
2023-03-10 16:57:33 +0800

    隐藏cookie

--- search/matcher/jd.go
+++ search/matcher/jd.go
@@ -3,6 +3,7 @@ package matcher
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/spf13/viper"
 	"io"
 	"log"
 	"net/http"
@@ -19,8 +20,6 @@ func init() {
 	search.Register("jd", matcher)
 }
 
-var cookie = "__jdv=67544842|www.bing.com|-|referral|-|1678182783563; __jdu=16781827835621828175748; areaId=2; ipLoc-djd=2-2813-0-0; PCSYCityID=CN_310000_310100_0; shshshfpb=oj0O0RwukqSp-gxqrUmdtfQ; shshshfpa=df894b94-4168-3f79-1527-6ef72b5c912e-1644222997; shshshfpx=df894b94-4168-3f79-1527-6ef72b5c912e-1644222997; rkv=1.0; pinId=xDiTk2JdIT0TdENyrdCC2w; pin=ibuy360com; unick=DayBuyDay_2012; _tp=8sidNCf8fyJUang0OEwlMw%3D%3D; _pst=ibuy360com; TrackID=1HTs0tkStxqEj4gAQiJAPw9M6ZV5CJwpg20FhQ6VAknt93w50qDZpR80jPQSXAAUd5KrGC17K3YNyJAXV8rykGZ1z3MNvxjvOqS958jD-vkNaz8T71AUMGHWgWRmFq8jh; qrsc=3; __jda=76161171.16781827835621828175748.1678182784.1678340561.1678345422.8; __jdb=76161171.3.16781827835621828175748|8.1678345422; __jdc=76161171; shshshfp=069559a3f4ee9fac6db47497f32cc9c7; shshshsID=9a6b6525d1b58f3cb40259d454081fff_1_1678345434341; 3AB9D23F7A4B3C9B=BOU5P7GIOES5LELENMJN4GXAIM3VDLWM56QAZBVGQAZKFU2DEBBGOH5WNJRS2A3LLUSRFSPQJQFBKJN3DXFC2YFULQ"
-
 type jdMatcher struct{}
 
 var header = http.Header{
@@ -31,7 +30,6 @@ var header = http.Header{
 	"accept":                    {"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
 	"accept-encoding":           {"gzip, deflate, br"},
 	"accept-language":           {"zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"},
-	"cookie":                    {cookie},
 	"referer":                   {"https://www.jd.com/"},
 	"sec-ch-ua":                 {"\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Microsoft Edge\";v=\"110\""},
 	"sec-ch-ua-mobile":          {"?0"},
@@ -58,6 +56,8 @@ func (m jdMatcher) Search(searchTerm string) ([]*model.Product, error) {
 		return nil, err
 	}
 
+	// 用不用cookie，查询结果还不一样
+	header.Set("cookie", viper.GetString("jd.cookie"))
 	request.Header = header
 	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
